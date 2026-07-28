51d18a8a9d234fb7b0541b29af5ea733260330d3
oguzb <hoguzbolat@gmail.com>
2026-07-28 13:21:21 +0300

    old model removed

--- src/model/cnn_train.py
+++ src/model/cnn_train.py
@@ -109,49 +109,6 @@ class ImprovedSeismicCNN(nn.Module):
         x = self.classifier(x)
         return x
 
-# Model Definition
-class SeismicCNN(nn.Module):
-    def __init__(self):
-        super(SeismicCNN, self).__init__()
-        
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
-        self.bn1 = nn.BatchNorm2d(32)
-        
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.bn2 = nn.BatchNorm2d(64)
-        
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.bn3 = nn.BatchNorm2d(128)
-        
-        self.conv4 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
-        self.bn4 = nn.BatchNorm2d(256)
-        
-        self.pool = nn.MaxPool2d(2, 2)
-        
-        # ATTENTION MODULE
-        # It takes the 256 channels from conv4 and re-weights them
-        self.attention = SEBlock(channels=256)
-        
-        self.global_pool = nn.AdaptiveAvgPool2d((1, 1))
-        self.dropout = nn.Dropout(0.4)
-        self.fc = nn.Linear(256, 1)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.bn1(self.conv1(x))))
-        x = self.pool(F.relu(self.bn2(self.conv2(x))))
-        x = self.pool(F.relu(self.bn3(self.conv3(x))))
-        
-        # Pass through the final conv layer
-        x = F.relu(self.bn4(self.conv4(x))) 
-        
-        x = self.attention(x)
-        
-        # Squash, flatten, and classify
-        x = self.global_pool(x)
-        x = torch.flatten(x, 1)
-        x = self.dropout(x)
-        x = self.fc(x)
-        return x
 
 
 if __name__ == "__main__":
